27150e54a0c87f8645483fb38a823f1ee8b37411
repl.it user <replituser@example.com>
2020-09-28 02:46:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -103,9 +103,23 @@ void * searchMap(HashMap * map,  char * key) {
 }
 
 void * firstMap(HashMap * map) {
+  unsigned long long a;
+  for (a = 0 ; a < map -> capacity ;a++) {
+    if (map -> buckets[a] != NULL) {
+      map -> current = a;
+      return map -> buckets[a] -> value;
+    }
+  }
   return NULL;
 }
 
 void * nextMap(HashMap * map) {
+  unsigned long long a;
+  for (a = 0 ; a < map -> capacity ;a++) {
+    if (map -> buckets[a + 1] != NULL) {
+      map -> current = a + 1;
+      return map -> buckets[a+ 1] -> value;
+    }
+  }
   return NULL;
 }
